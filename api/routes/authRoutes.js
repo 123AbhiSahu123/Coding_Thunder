11cd272a0710d50d5a6d6a7c5ab20c1c6dd6c3f6
123AbhiSahu123 <sahuabhishek3810@gmail.com>
2026-01-29 22:43:51 +0530

    ui make atractive

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -29,16 +29,3 @@ router.get("/logout", logoutUser);
 module.exports = router;
 
 
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   loginPage,
-//   loginUser,
-//   logoutUser,
-// } = require("../controllers/authController");
-
-// router.get("/login", loginPage);
-// router.post("/login", loginUser);
-// router.get("/logout", logoutUser);
-
-// module.exports = router;
